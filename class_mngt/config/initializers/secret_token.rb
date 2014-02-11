6c7ad1035485f7b5233d0a954c5caa3b9d12a846
Thomas G. Petrie <tgpetrie@gmail.com>
2014-02-10 19:58:01 -0800

    over writing the deault rails parge with an index.html page in public for the 'root' or '/' address

---
+++ class_mngt/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClassMngt::Application.config.secret_key_base = '6b47ac0e65048a29a7e2f1cfb98ac551b45934511763bcd40417c85bfdac010cbd1c55e450fee44a1bca312c6d568225642ec13ad5bbb8c54aa30c0dfc4cbf8c'
